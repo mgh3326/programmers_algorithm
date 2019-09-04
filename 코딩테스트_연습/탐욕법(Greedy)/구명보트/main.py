626149179d7e12e24fc7589d63013d2178f3b1b9
kwanghyunMoon <mgh3326@naver.com>
2019-09-04 22:29:27 +0900

    평균값인 경우 조건 제거

--- 코딩테스트_연습/탐욕법(Greedy)/구명보트/main.py
+++ 코딩테스트_연습/탐욕법(Greedy)/구명보트/main.py
@@ -5,22 +5,10 @@ def solution(people, limit):
     answer = 0
     person_list = []
     person_desc__list = []
-    average = limit // 2
-    is_to_add = False
-    people_count = people.count(average)
-    answer += people_count // 2
-    if people_count % 2 == 1:
-        is_to_add = True
 
     for person in people:
-        if person == average:
-            pass
-        else:
-            heapq.heappush(person_list, person)
-            heapq.heappush(person_desc__list, person * -1)
-    if is_to_add == True:  # 에버리지 값은 짝수 개를 지워야하므로 일단 다 지움
-        heapq.heappush(person_list, average)
-        heapq.heappush(person_desc__list, average * -1)
+        heapq.heappush(person_list, person)
+        heapq.heappush(person_desc__list, person * -1)
     answer += len(person_list)
     if len(person_list) == 0 or len(person_list) == 1:
         return answer
@@ -42,7 +30,7 @@ def solution(people, limit):
             person_desc__list.remove(person_min * -1)
 
         else:  # 못들어간다리 Max만 조신히 들어가도록 하자
-            if len(person_list) == 0 or len(person_list) == 1:
+            if len(person_list) == 0:
                 return answer
             person_max = heapq.heappop(person_desc__list) * -1
             person_list.remove(person_max)
@@ -50,16 +38,16 @@ def solution(people, limit):
     return answer
 
 
-print(
-    solution(
-        [40, 40, 40], 100
-    )
-)
 # print(
 #     solution(
-#         [70, 50, 80, 50], 100
+#         [40, 40, 40], 100
 #     )
 # )
+print(
+    solution(
+        [70, 50, 80, 50], 100
+    )
+)
 # print(
 #     solution(
 #         [70, 80, 50], 100

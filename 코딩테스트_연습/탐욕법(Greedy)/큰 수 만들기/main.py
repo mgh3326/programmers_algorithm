1051819054c2027934dff9de33d289ecf80ed654
kwanghyunMoon <mgh3326@naver.com>
2019-09-04 21:42:02 +0900

    complete

--- 코딩테스트_연습/탐욕법(Greedy)/큰 수 만들기/main.py
+++ 코딩테스트_연습/탐욕법(Greedy)/큰 수 만들기/main.py
@@ -1,8 +1,10 @@
 import heapq
 
 
+# TODO 스택을 이용하면 아주 효율적으로 문제를 풀수 있다.
 def solution(number, k):
     answer = ''
+    stack = [number[0]]
     number_str = str(number)
     my_number_list = list(number_str)
 
@@ -34,30 +36,38 @@ def solution(number, k):
     remove_save_list = []
     for i in range(k):
         if len(remove_list) == 0:
-            heapq.heappush(remove_save_list, 1)
+            break
 
         else:
             heappop = remove_list.pop(0)
             # heappop = heapq.heappop(remove_list)
-            heapq.heappush(remove_save_list, -1 * heappop[0])
+            heapq.heappush(remove_save_list, heappop[0])
+    current_index = 0
+    heapq_heappop = 0
     while True:
         if len(remove_save_list) == 0:
             break
-        heapq_heappop = heapq.heappop(remove_save_list) * -1
-        if heapq_heappop == -1:
-            my_number_list.pop()
-        else:
-            my_number_list.pop(heapq_heappop)
-    answer = ''.join(my_number_list)
+        heapq_heappop = heapq.heappop(remove_save_list)
+        while True:
+            if current_index != heapq_heappop:
+                answer += number_str[current_index]
+                current_index += 1
+            else:
+                current_index += 1
+                break
+    if current_index == 0 and heapq_heappop == 0:
+        answer += number_str[:len(number_str) - k]
+    else:
+        answer += number_str[heapq_heappop + 1:heapq_heappop + 1 + len(number_str) - k]
     return answer
 
 
 # # # 94
-# print(
-#     solution(
-#         1231234, 5
-#     )
-# )
+print(
+    solution(
+        9111, 2
+    )
+)
 # 3234
 print(
     solution(

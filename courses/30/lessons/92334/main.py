bcb5da8bec65e21111316c3e4a2d7f540a60a55b
robin <robin@watcha.com>
2022-07-23 19:18:54 +0900

    내용을 관과하여 오래 걸렸다. (유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다)

--- courses/30/lessons/92334/main.py
+++ courses/30/lessons/92334/main.py
@@ -1,27 +1,21 @@
 def solution(id_list, report, k):
-    result_hash = {}
     reported_hash = {}
-    send_hash = {}
+    result_hash = {}
 
     for _id in id_list:
-        result_hash[_id] = 0
         reported_hash[_id] = set()
-        send_hash[_id] = set()
+        result_hash[_id] = 0
 
     for r in report:
         reporter, reported = r.split(" ")
-
         reported_hash[reported].add(reporter)
-        if len(reported_hash[reported]) >= k:
-            for _reporter in reported_hash[reported]:
-                if reported in send_hash[_reporter]:
-                    continue
-                result_hash[_reporter] += 1
-                send_hash[_reporter].add(reported)
-    answer = []
-    for value in send_hash.values():
-        answer.append(len(value))
-    return answer
+
+    for reported_hash_key in reported_hash.keys():
+        reported_hash_value = reported_hash[reported_hash_key]
+        if len(reported_hash_value) >= k:
+            for _id in reported_hash_value:
+                result_hash[_id] += 1
+    return list(result_hash.values())
 
 
 def test_solution1():
@@ -31,3 +25,7 @@ def test_solution1():
 
 def test_solution2():
     assert solution(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3) == [0, 0]
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=python3
+# 유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다.
+# 위 내용을 관과하여 오래 걸렸다.

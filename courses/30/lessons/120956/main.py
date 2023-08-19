3fbd5bc4109cbad37437913d0729273070f0f999
robin <robin@watcha.com>
2023-08-19 18:42:44 +0900

    Simplify pronunciation validation function
    Cleaned up the `has_valid_pronunciations` function by removing unnecessary variable index and removing the need for deepcopy import. This is achieved by using string slicing and list slicing directly in the recursive function call. Reduction of complexity leads to more efficient computing. It simplifies the logic and makes the code easier to read.

--- courses/30/lessons/120956/main.py
+++ courses/30/lessons/120956/main.py
@@ -1,17 +1,10 @@
-import copy
-
-
 def has_valid_pronunciations(value, possible_pronunciations):
-    idx = 0
-    if idx == len(value):
+    if not value:
         return True
-    current_segment = value[idx:]
     for i, pronunciation in enumerate(possible_pronunciations):
-        if current_segment.startswith(pronunciation):
-            idx += len(pronunciation)
-            deepcopy = copy.deepcopy(possible_pronunciations)
-            deepcopy.pop(i)
-            if has_valid_pronunciations(value[idx:], deepcopy):
+        if value.startswith(pronunciation):
+            if has_valid_pronunciations(value[len(pronunciation):],
+                                        possible_pronunciations[:i] + possible_pronunciations[i + 1:]):
                 return True
     return False
 
@@ -20,6 +13,6 @@ def solution(babblings):
     possible_pronunciations = ["aya", "ye", "woo", "ma"]
     valid_babblings = 0
     for babble in babblings:
-        if has_valid_pronunciations(babble, copy.deepcopy(possible_pronunciations)):
+        if has_valid_pronunciations(babble, possible_pronunciations):
             valid_babblings += 1
     return valid_babblings

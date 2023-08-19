27c5fb1f6e05f1f57b0468921fcc29b9ced8ee03
robin <robin@watcha.com>
2023-08-20 02:32:47 +0900

    Refactor and improve is_composite function
    Updated the is_composite function for better efficiency and made it more descriptive. Firstly, the parameter name has been changed from 'n' to 'number' for clarity. Secondly, added a docstring to provide a clear definition of what a composite number is. Lastly, the function now checks for numbers greater than 3 as any number less than 4 can not be composite. This reduces unnecessary calculations. This change also reflected in the solution function where the range for the loop starts from 4 instead of 1.

--- courses/30/lessons/120956/main.py
+++ courses/30/lessons/120956/main.py
@@ -1,12 +1,17 @@
-def is_composite_number(n):
-    if n < 2:
+def is_composite(number):
+    """
+    This function checks if a number is composite.
+    A composite number is a positive integer that has at least one positive divisor other than one or itself.
+    In other words, a composite number is any positive integer greater than one that is not a prime number.
+    """
+    if number < 4:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
             return True
     return False
 
 
 def solution(n):
-    answer = sum(is_composite_number(i + 1) for i in range(n))
+    answer = sum(is_composite(i) for i in range(4, n + 1))
     return answer

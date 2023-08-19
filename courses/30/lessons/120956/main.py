af42dc8247079cb82eecb80e054d67c891255745
robin <robin@watcha.com>
2023-08-19 18:38:32 +0900

    한번씩만 쓰일수 있네

--- courses/30/lessons/120956/main.py
+++ courses/30/lessons/120956/main.py
@@ -1,20 +1,25 @@
-def dfs(idx, value, possible_pronunciations):
+import copy
+
+
+def has_valid_pronunciations(value, possible_pronunciations):
+    idx = 0
     if idx == len(value):
         return True
-    current_value = value[idx:]
-    for possible_pronunciation in possible_pronunciations:
-        if current_value.startswith(possible_pronunciation):
-            idx += len(possible_pronunciation)
-            if dfs(idx, value, possible_pronunciations):
+    current_segment = value[idx:]
+    for i, pronunciation in enumerate(possible_pronunciations):
+        if current_segment.startswith(pronunciation):
+            idx += len(pronunciation)
+            deepcopy = copy.deepcopy(possible_pronunciations)
+            deepcopy.pop(i)
+            if has_valid_pronunciations(value[idx:], deepcopy):
                 return True
     return False
 
 
-def solution(babbling):
+def solution(babblings):
     possible_pronunciations = ["aya", "ye", "woo", "ma"]
-    result = 0
-    for value in babbling:
-        idx = 0
-        if dfs(idx, value, possible_pronunciations):
-            result += 1
-    return result
+    valid_babblings = 0
+    for babble in babblings:
+        if has_valid_pronunciations(babble, copy.deepcopy(possible_pronunciations)):
+            valid_babblings += 1
+    return valid_babblings

cac1274dd6c53f726cbed1975258e78f4589a25a
robin <robin@watcha.com>
2023-10-09 16:46:10 +0900

    Add solution and tests for lesson 72413
    Added a Python solution for lesson 72413. The solution employs a Floyd-Warshall algorithm to find the minimum cost between any two points in a directed, weighted graph. This is particularly applicable to graph problems involving shortest paths. Tests were also added to confirm the correctness of the solution, covering different types of fare structures.

---
+++ courses/30/lessons/72413/main.py
@@ -0,0 +1,28 @@
+def solution(n, s, a, b, fares):
+    INF = float("inf")
+    graph = [[0] * n for _ in range(n)]
+    for fare in fares:
+        p1, p2, cost = fare
+        graph[p1 - 1][p2 - 1] = cost
+        graph[p2 - 1][p1 - 1] = cost
+    for i in range(n):
+        for j in range(n):
+            if i != j and graph[i][j] == 0:
+                graph[i][j] = INF
+    next_nodes = [[None if i == j else j for j in range(n)] for i in range(n)]
+
+    # Floyd-Warshall algorithm
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if graph[i][k] + graph[k][j] < graph[i][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
+                    next_nodes[i][j] = next_nodes[i][k]
+
+    def cost(start, end):
+        return graph[start - 1][end - 1]
+
+    min_cost = 40000002
+    for i in range(1, n + 1):
+        min_cost = min(min_cost, cost(s, i) + cost(i, a) + cost(i, b))
+    return min_cost

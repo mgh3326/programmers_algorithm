bbea2ea2f7301c15f360b48873a26e7ae0c8be46
robin <robin@watcha.com>
2025-07-12 19:43:19 +0900

    Refactor BFS logic for lesson 169199
    Simplified the `solution` function by restructuring the BFS implementation and replacing repetitive direction-specific logic with a unified loop. Improved code readability and reduced redundancy.

--- courses/30/lessons/169199/main.py
+++ courses/30/lessons/169199/main.py
@@ -1,67 +1,40 @@
+from collections import deque
+
+
 def solution(board):
-    d_values_set = set()
-    for h in range(len(board)):
-        for w in range(len(board[h])):
-            if board[h][w] == "R":
-                r_values = (h, w)
-            elif board[h][w] == "G":
-                g_values = (h, w)
-            elif board[h][w] == "D":
-                d_values_set.add(tuple([h, w]))
     h_len = len(board)
     w_len = len(board[0])
-    # BFS 접근이라고 생각이 든다
-    directions = ["U", "D", "L", "R"]
-    queue = []  # h, w, direction_idx, depth
-    current_h, current_w = r_values
-    g_h, g_w = g_values
-    visited = set()
-    queue.append((current_h, current_w, 0))
-    while True:
-        if len(queue) == 0:
-            answer = -1
-            break
-        current_h, current_w, depth = queue.pop(0)
-        if current_h == g_h and current_w == g_w:
-            answer = depth
-            break
-        for direction_inx in range(len(directions)):
-            direction = directions[direction_inx]
-            if direction == "U":
-                next_h = current_h
-                while True:
-                    next_h -= 1
-                    if next_h < 0 or (next_h, current_w) in d_values_set:
-                        if next_h + 1 != current_h and (next_h + 1, current_w) not in visited:
-                            visited.add((next_h + 1, current_w))
-                            queue.append((next_h + 1, current_w, depth + 1))
-                        break
-            elif direction == "D":
-                next_h = current_h
-                while True:
-                    next_h += 1
-                    if next_h >= h_len or (next_h, current_w) in d_values_set:
-                        if next_h - 1 != current_h and (next_h - 1, current_w) not in visited:
-                            visited.add((next_h - 1, current_w))
-                            queue.append((next_h - 1, current_w, depth + 1))
-                        break
-            elif direction == "L":
-                next_w = current_w
-                while True:
-                    next_w -= 1
-                    if next_w < 0 or (current_h, next_w) in d_values_set:
-                        if next_w + 1 != current_w and (current_h, next_w + 1) not in visited:
-                            visited.add((current_h, next_w + 1))
-                            queue.append((current_h, next_w + 1, depth + 1))
-                        break
-            elif direction == "R":
-                next_w = current_w
-                while True:
-                    next_w += 1
-                    if next_w >= w_len or (current_h, next_w) in d_values_set:
-                        if next_w - 1 != current_w and (current_h, next_w - 1) not in visited:
-                            visited.add((current_h, next_w - 1))
-                            queue.append((current_h, next_w - 1, depth + 1))
-                        break
+    obstacles = set()
 
-    return answer
+    for h in range(h_len):
+        for w in range(w_len):
+            if board[h][w] == "R":
+                start = (h, w)
+            elif board[h][w] == "G":
+                goal = (h, w)
+            elif board[h][w] == "D":
+                obstacles.add((h, w))
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우
+    queue = deque()
+    visited = set()
+
+    queue.append((*start, 0))  # (h, w, depth)
+    visited.add(start)
+
+    while queue:
+        h, w, depth = queue.popleft()
+        if (h, w) == goal:
+            return depth
+        for dx, dy in directions:
+            nh, nw = h, w
+            while True:
+                th, tw = nh + dx, nw + dy
+                if not (0 <= th < h_len and 0 <= tw < w_len): break
+                if (th, tw) in obstacles: break
+                nh, nw = th, tw
+            if (nh, nw) not in visited:
+                visited.add((nh, nw))
+                queue.append((nh, nw, depth + 1))
+
+    return -1

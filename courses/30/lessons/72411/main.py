62199d06d52f17f2963d7ee8f87e0d4f6d01a584
robin <robin@watcha.com>
2025-07-15 11:53:31 +0900

    Refactor solution for lesson 72411
    Simplified course combination logic using `collections.Counter` to optimize counting and reduce redundancy. Improved code clarity and efficiency.

--- courses/30/lessons/72411/main.py
+++ courses/30/lessons/72411/main.py
@@ -1,31 +1,18 @@
 import itertools
+from collections import Counter
 
 
 def solution(orders, course):
     answer = []
     for course_value in course:
-        comb_set_list = []
-        max_count = 0
-        comb_hash = {}
-        temp_answer_list = []
-        for i in range(len(orders)):
-            order = orders[i]
-            comb_set = set(itertools.combinations(sorted(order), course_value))
-            comb_set_list.append(comb_set)
-        for i in range(len(orders)):
-            for j in range(i + 1, len(orders)):
-                if i == j:
-                    continue
-                for value in list(comb_set_list[i] & comb_set_list[j]):
-                    comb_hash[value] = comb_hash.get(value, 0) + 1
-                    if comb_hash[value] > max_count:
-                        max_count = comb_hash[value]
-                        temp_answer_list = [value]
-                    elif comb_hash[value] == max_count:
-                        temp_answer_list.append(value)
-        answer.append(temp_answer_list)
-    result = []
-    for values in answer:
-        for value in values:
-            result.append("".join(value))
-    return sorted(result)
+        comb_counter = Counter()
+        for order in orders:
+            combs = (itertools.combinations(sorted(order), course_value))
+            comb_counter.update(combs)
+        if comb_counter:
+            max_order_count = max(comb_counter.values())
+            if max_order_count >= 2:
+                for order, count in comb_counter.items():
+                    if count == max_order_count:
+                        answer.append("".join(order))
+    return sorted(answer)

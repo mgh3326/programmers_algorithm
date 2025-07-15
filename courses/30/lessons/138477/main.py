6c171d73a7cb2b8a78065bf0c3cca0f1976e08fc
robin <robin@watcha.com>
2025-07-15 12:18:23 +0900

    Refactor solution for lesson 138477
    Simplified heap operations by replacing redundant logic with `heapreplace`. Improved code clarity and reduced unnecessary variable tracking.

--- courses/30/lessons/138477/main.py
+++ courses/30/lessons/138477/main.py
@@ -4,23 +4,11 @@ import heapq
 def solution(k, score):
     answer = []
     heap = []
-    heap_min_score = score[0]
-    heap_max_score = score[0]
     for x in score:
         if len(heap) >= k:
-            if heap_min_score < x:
-                heapq.heappop(heap)
-                heapq.heappush(heap, x)
-                # heapreplace가 동일한가?
-
-                heap_min_score = heapq.heappop(heap)
-                heapq.heappush(heap, heap_min_score)
-                #넣다 빼는것 말고 제일 작은 값을 확인할수 있나?
+            if heap[0] < x:
+                heapq.heapreplace(heap, x)
         else:
             heapq.heappush(heap, x)
-            if heap_min_score > x:
-                heap_min_score = x
-            elif heap_max_score < x:
-                heap_max_score = x
-        answer.append(heap_min_score)
+        answer.append(heap[0])
     return answer

f9d469e70903ca08d0cd900a914bffe1ca6bae0d
robin <robin@watcha.com>
2025-07-17 19:20:38 +0900

    Refactor solution for lesson 138476
    Simplified `Counter` logic and iteration to improve code clarity and reduce redundancy.

--- courses/30/lessons/138476/main.py
+++ courses/30/lessons/138476/main.py
@@ -2,13 +2,11 @@ from collections import Counter
 
 
 def solution(k, tangerine):
+    tangerine_counter = Counter(tangerine)
     answer = 0
-    tangerine_counter = Counter()
-    tangerine_counter.update(tangerine)
-    for value in sorted(tangerine_counter.values(), reverse=True):
-        k -= value
-        answer+=1
+    for count in sorted(tangerine_counter.values(), reverse=True):
+        k -= count
+        answer += 1
         if k <= 0:
             break
-
     return answer
